08497f5480c2d4998680dc76e3990e70a04c9fbe
Finnyins <mataoasis@gmail.com>
2022-02-24 10:28:55 -0700

    Added Beta Business Site

---
+++ BusinessSiteProject/scripts.js
@@ -0,0 +1,7 @@
+function NavSlideOut() {
+    document.getElementById("NavSlider").style.width = "250px";
+  }
+
+function NavSlideIn() {
+    document.getElementById("NavSlider").style.width = "0";
+}

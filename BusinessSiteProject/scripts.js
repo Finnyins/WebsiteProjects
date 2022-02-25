96fc504e9fdd0e6937561029374e1c57ae632abd
Finnyins <mataoasis@gmail.com>
2022-02-25 08:19:26 -0700

    Added store page for MCG

--- BusinessSiteProject/scripts.js
+++ BusinessSiteProject/scripts.js
@@ -1,6 +1,6 @@
 function NavSlideOut() {
     document.getElementById("NavSlider").style.width = "250px";
-  }
+}
 
 function NavSlideIn() {
     document.getElementById("NavSlider").style.width = "0";
